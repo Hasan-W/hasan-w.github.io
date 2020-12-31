a861d437150b1fa75cbe04188fd176ebe7b2abd6
Hasan-W <hasan.wang520@gmail.com>
2020-12-31 20:29:56 +0800

    Site updated: 2020-12-31 20:29:55

--- js/main.js
+++ js/main.js
@@ -1,16 +1,49 @@
-window.addEventListener('DOMContentLoaded', () => {
-  // print theme info
-  ILS.utils.printThemeInfo();
+(function($){
+    $('.article img:not(".not-gallery-item")').each(function () {
+        // wrap images with link and add caption if possible
+        if ($(this).parent('a').length === 0) {
+            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+            if (this.alt) {
+                $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
+            }
+        }
+    });
 
-  // init scroll
-  ILS.utils.registerWindowScroll();
+    if (typeof(moment) === 'function') {
+        $('.article-meta time').each(function () {
+            $(this).text(moment($(this).attr('datetime')).fromNow());
+        });
+    }
+    $('.article > .content > table').each(function () {
+        if ($(this).width() > $(this).parent().width()) {
+            $(this).wrap('<div class="table-overflow"></div>');
+        }
+    });
+    function adjustNavbar() {
+        const navbarWidth = $('.navbar-main .navbar-start').outerWidth() + $('.navbar-main .navbar-end').outerWidth();
+        if ($(document).outerWidth() < navbarWidth) {
+            $('.navbar-main .navbar-menu').addClass('is-flex-start');
+        } else {
+            $('.navbar-main .navbar-menu').removeClass('is-flex-start');
+        }
+    }
+    adjustNavbar();
+    $(window).resize(adjustNavbar);
 
-  // init tools button
-  ILS.utils.registerToolsButtonClick();
+    var $toc = $('#toc');
+    if ($toc.length > 0) {
+    var $mask = $('<div>');
+        $mask.attr('id', 'toc-mask');
 
-  // comment
-  ILS.utils.goComment();
+        $('body').append($mask);
 
-  // init page height handle
-  ILS.utils.initPageHeightHandle();
-});
+        function toggleToc() {
+            $toc.toggleClass('is-active');
+            $mask.toggleClass('is-active');
+        }
+
+        $toc.on('click', toggleToc);
+        $mask.on('click', toggleToc);
+        $('.navbar-main .catalogue').on('click', toggleToc);
+    }
+})(jQuery);
